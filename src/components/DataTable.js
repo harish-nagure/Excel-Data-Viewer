006aab745f927c642cc3c14f32b20eb66b5a9bda
Harish Nagure <nagureharish@gmail.com>
2025-01-28 15:38:18 +0530

    Submit

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -81,7 +81,7 @@ const DataTable = ({ data }) => {
     </div>
     
       <button 
-      onClick={()=>{navigate("/db")}}
+      onClick={()=>{navigate("/dashboard")}}
       className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'
       >Dashboard</button>
     </>
